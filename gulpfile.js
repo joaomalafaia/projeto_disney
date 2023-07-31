42f07d75bbfab6672d77777f9230418c26e78c42
João Malafaia <j.malafaia02@gmail.com>
2023-07-30 23:14:07 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 function styles() { /*aqui estamos avisando à máquina para compilar e transofrmas o scss em css*/
     return gulp.src('./src/styles/*.scss')
@@ -14,7 +21,8 @@ function images() { /*aqui estamos avisando à máquina para minimizar as imagen
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images); /* forma de colocar o watch para atualização simultânea do desenvolvimento */
+exports.default = gulp.parallel(styles, images, scripts); /* forma de colocar o watch para atualização simultânea do desenvolvimento */
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*js', gulp. parallel(scripts));
 }
